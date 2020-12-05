b1c60552927dca6c191e413d153efac43e63e293
unknown <pedr20ab@student.cbs.dk>
2020-12-05 18:45:21 +0100

    024 matching function kinda functional

--- Module/server.js
+++ Module/server.js
@@ -16,13 +16,54 @@ const { error } = require("console");
 
 server.post("/user", signUpController);
 
+
 const loginController = require('../Controller/loginController')
 
 server.post("/login", loginController);
 
 
-
-
 const deleteController = require('../Controller/deleteController')
 
 server.delete("/deleteprofile", deleteController)
+
+
+server.post("/likeUser", (req, res, next) => {
+
+    let likeArray = JSON.parse(fs.readFileSync('../Storage/likeUserArray.json'))
+    
+    
+
+    likeArray.push(req.body.Username)
+
+    fs.writeFile('../Storage/likeUserArray.json', JSON.stringify(likeArray, null, 4), (err) => {
+        if (err) throw err;
+    })
+
+    let userArray = JSON.parse(fs.readFileSync('../Storage/user1.json'));
+
+    let checking = true;
+
+    let checking2 = false;
+
+    let checking3 = false;
+
+    for (var i = 0; i < req.body.Liked.length; i++) {
+        for (var j = 0; j < userArray.length; j++) {
+            if (req.body.Liked[i] == userArray[j].Username) {
+                checking2 = true
+            }
+        }
+    }
+
+    for(var i=0; i<likeArray.length; i++){
+        if(req.body.Username == likeArray[i]) {
+            
+            checking3 = true
+        }
+    } if(checking2 == true && checking3 == true){
+        res.json("It's a match")
+    } else if(checking == true) {
+        res.json("Fail")
+    }
+
+})
--- View/Users.js
+++ View/Users.js
@@ -1,18 +1,23 @@
 class  User{
-    constructor(firstName, lastName, age, gender){
+    constructor(firstName, lastName, age, gender, userName, liked){
         this.firstName=firstName;
         this.lastName=lastName;
         this.age=age;
         this.gender=gender;
+        this.userName=userName;
+        this.liked=liked;
     }
 }
 
+let loggedinID = JSON.parse(localStorage.getItem("userID"))
 
-let Oscar = new User("Oscar", "Pedersen", 20, "Male");
-let Mike = new User ("Mike", "Jensen", 21, "Male");
-let Christian = new User("Christian", "Bredgaard", 20, "Male");
-let Anna = new User("Anna", "Nielsen", 22, "Female");
-let Kristine = new User("Kristine", "Andersen", 21, "Female");
+
+
+let Oscar = new User("Oscar", "Pedersen", 20, "Male", "SQLguy123", [loggedinID.Username, "Mike", "Christian"]);
+let Mike = new User ("Mike", "Jensen", 21, "Male", "Magic22", []);
+let Christian = new User("Christian", "Bredgaard", 20, "Male", "Breadguard44", []);
+let Anna = new User("Anna", "Nielsen", 22, "Female", "Lorem77", []);
+let Kristine = new User("Kristine", "Andersen", 21, "Female", "Ipsum88", []);
 
 
 let otherUsers = [Oscar, Mike, Christian, Anna, Kristine];
@@ -24,17 +29,21 @@ const genderID = document.getElementById("genderID");
 
 
 
-window.onload = 
+window.onload = function loadFirst(){
 firstnameID.innerHTML = otherUsers[0].firstName;
 lastnameID.innerHTML = otherUsers[0].lastName;
 ageID.innerHTML = otherUsers[0].age;
 genderID.innerHTML = otherUsers[0].gender;
 
+}
 
-let i=1
+
+
+
+let i=0
 
 function goNext(){
-    
+    i++
     if(i>=otherUsers.length){
         console.log(otherUsers[i])
         alert("Det er ikke flere at matche med. Tryk ok for at vende tilbage til forsiden.")
@@ -43,7 +52,6 @@ function goNext(){
     } else if (i<otherUsers.length){
         console.log(otherUsers[i])
         showUser(i);
-        return i++
     }
 }
 
@@ -54,4 +62,28 @@ function showUser(){
     lastnameID.innerHTML = otherUsers[i].lastName
     ageID.innerHTML = otherUsers[i].age
     genderID.innerHTML = otherUsers[i].gender
-}
+};
+
+
+function likeUser(){
+    let likedUser = {
+        Username: otherUsers[i].firstName,
+        Liked: otherUsers[i].liked
+    }
+    console.log(likedUser);
+       fetch('http://localhost:3000/likeUser', {
+     method: 'POST', // or 'PUT'
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(likedUser),
+   }).then(res => res.json())
+   .then(data => {  
+     if(data == "It's a match"){
+         alert("It's a match! Go to 'My matches' to see more.")
+     } 
+   })
+   .catch((error) => {
+     console.error('Error:', error);
+   })
+   }
